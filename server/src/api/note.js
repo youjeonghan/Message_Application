47ccf960b4d6860bf19fb387429d8494375570ff
coding_egg <yjh9360@naver.com>
2022-03-18 23:03:07 +0900

    Feat: 메모 수정 API 구현

--- server/src/api/note.js
+++ server/src/api/note.js
@@ -6,5 +6,6 @@ const noteController = require('../controller/note_controller');
 
 note.get('/', noteController.readAllNote);
 note.post('/', authController.certify, noteController.createNote);
+note.patch('/:id', authController.certify, noteController.updateNote);
 
 module.exports = note;
--- server/src/controller/note_controller.js
+++ server/src/controller/note_controller.js
@@ -23,3 +23,22 @@ exports.readAllNote = async (ctx) => {
   }
   ctx.status = 200;
 };
+
+exports.updateNote = async (ctx) => {
+  const { id: noteId } = ctx.params;
+  const { content } = ctx.request.body;
+  const { id: userId } = await ctx.state.user;
+  try {
+    const note = (await db.note.update(
+      { content },
+      { where: { id: noteId, user_id: userId } },
+    ))[0];
+    if (note !== 0) {
+      ctx.status = 200; // 정상적인 요청
+    } else {
+      ctx.status = 400; // 잘못된 요청
+    }
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+};
