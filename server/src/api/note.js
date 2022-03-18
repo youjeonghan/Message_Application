009b90bc75526a0cb1f4d829adc0379cb5ac66a6
coding_egg <yjh9360@naver.com>
2022-03-18 23:10:16 +0900

    Feat: 메모 삭제 API 구현

--- server/src/api/note.js
+++ server/src/api/note.js
@@ -7,5 +7,6 @@ const noteController = require('../controller/note_controller');
 note.get('/', noteController.readAllNote);
 note.post('/', authController.certify, noteController.createNote);
 note.patch('/:id', authController.certify, noteController.updateNote);
+note.delete('/:id', authController.certify, noteController.deleteNote);
 
 module.exports = note;
--- server/src/controller/note_controller.js
+++ server/src/controller/note_controller.js
@@ -29,11 +29,28 @@ exports.updateNote = async (ctx) => {
   const { content } = ctx.request.body;
   const { id: userId } = await ctx.state.user;
   try {
-    const note = (await db.note.update(
+    const result = (await db.note.update(
       { content },
       { where: { id: noteId, user_id: userId } },
     ))[0];
-    if (note !== 0) {
+    if (result !== 0) {
+      ctx.status = 200; // 정상적인 요청
+    } else {
+      ctx.status = 400; // 잘못된 요청
+    }
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+};
+
+exports.deleteNote = async (ctx) => {
+  const { id: noteId } = ctx.params;
+  const { id: userId } = await ctx.state.user;
+  try {
+    const result = await db.note.destroy(
+      { where: { id: noteId, user_id: userId } },
+    );
+    if (result !== 0) {
       ctx.status = 200; // 정상적인 요청
     } else {
       ctx.status = 400; // 잘못된 요청
