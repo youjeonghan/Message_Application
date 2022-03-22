c52c83f49b1e87860f617c0e80fec446667be506
coding_egg <yjh9360@naver.com>
2022-03-22 19:58:33 +0900

    Refactor: 로그인에 따른 redirect 수정

--- server/src/controller/user_controller.js
+++ server/src/controller/user_controller.js
@@ -1,31 +1,27 @@
 const passport = require('koa-passport');
-const db = require('../models/index');
 
 exports.signUser = (ctx) => passport.authenticate(
   'sign',
-  {
-    successRedirect: '/',
-    failureRedirect: '/',
-  },
   (err, user) => {
     if (user === false) {
-      ctx.throw(409);
+      ctx.status = 409;
+      ctx.redirect('/sign');
     }
     ctx.status = 201;
+    ctx.redirect('/');
   },
 )(ctx);
 
 exports.login = (ctx) => passport.authenticate(
   'login',
-  {
-    successRedirect: '/',
-    failureRedirect: '/',
-  },
   (err, user) => {
     if (user === false) {
-      ctx.throw(401);
+      ctx.status = 401;
+      ctx.redirect('/login');
     }
+    ctx.cookies.set('userId', user.name, { httpOnly: false });
     ctx.status = 200;
+    ctx.redirect('/');
     return ctx.login(user);
   },
 )(ctx);
